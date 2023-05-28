3b525a2450c45d6a7f404728eff136e9a86abba6
Minh Duong <64678096+minhnhat1901@users.noreply.github.com>
2023-05-27 19:44:03 -0700

    Entire lab

--- README.md
+++ README.md
@@ -6,3 +6,14 @@
 
 Explanation: I think this is the best option because it will run the tests whenever code is pushed, so it will be run often and will give the feedback on the changes. It will also be run before the code is merged into the main branch, so it will prevent any errors from being merged into the main branch. If the tests fail, the developer can quickly diagnose and correct the issues before those changes progress further down the development pipeline. On the other hand, Github Action is easy to use and can be set up quickly. It also provides a lot of flexibility in terms of what tests to run and when to run them. It's from Github so we don't need any additional tools or software to run the tests. Lastly, we can reduce the amount of time spent on manual testing and focus on other tasks.
 
+2) Would you use an end to end test to check if a function is returning the correct output? (yes/no)
+
+-> No because it is not a good practice to use end to end tests to check if a function is returning the correct output. Also, I believe that unit tests are better suited for this purpose.
+
+3) Would you use a unit test to test the “message” feature of a messaging application? Why or why not? For this question, assume the “message” feature allows a user to write and send a message to another user.
+
+-> It's not efficient when we use unit tests to test the "message" feature of a messaging application. Unit tests are used to test the smallest unit of code, which is a function. The "message" feature is a complex feature that involves multiple functions and components. Therefore, it's better to use end-to-end tests to test the "message" feature.
+
+4) Would you use a unit test to test the “max message length” feature of a messaging application? Why or why not? For this question, assume the “max message length” feature prevents the user from typing more than 80 characters.
+
+-> No, I would not use a unit test to test the "max message length" feature of a messaging application. The "max message length" feature is a complex feature that involves multiple functions and components. Also, the length is more than 80 characters, which might be very long message that if we use end-to-end testing, it's much efficient than unit testing. Therefore, it's better to use end-to-end tests to test the "max message length" feature.
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -1,5 +1,89 @@
 // unit.test.js
 
 const functions = require('../code-to-unit-test/unit-test-me.js');
-
 // TODO - Part 2
+
+// Phone number validation tests
+test('(000) 000-0000 is the valid phone number', () => {
+    expect(functions.isPhoneNumber('(000) 000-0000')).toBe(true);
+});
+
+test('111-111-1111 is the valid phone number', () => {
+    expect(functions.isPhoneNumber('111-111-1111')).toBe(true);
+});
+
+test('100 is not a valid phone number', () => {
+    expect(functions.isPhoneNumber('100')).toBe(false);
+});
+
+test('xyz is not a valid phone number', () => {
+    expect(functions.isPhoneNumber('xyz')).toBe(false);
+});
+
+// Email validation tests
+test('abc@gmail.com is a valid email', () => {
+    expect(functions.isEmail('abc@gmail.com')).toBe(true);
+});
+
+test('johndoe@ucsd.edu is a valid email', () => {
+    expect(functions.isEmail('johndoe@ucsd.edu')).toBe(true);
+});
+
+test('abc@gmail is not a valid email', () => {
+    expect(functions.isEmail('abc@gmail')).toBe(false);
+});
+
+test('abc is not a valid email', () => {
+    expect(functions.isEmail('abc')).toBe(false);
+});
+
+// Strong password validation tests
+test('abc123 is a strong valid password', () => {
+    expect(functions.isStrongPassword('abc123')).toBe(true);
+});
+
+test('A112233 is a strong valid password', () => {
+    expect(functions.isStrongPassword('A112233')).toBe(true);
+});
+
+test('abc is not a strong valid password', () => {
+    expect(functions.isStrongPassword('abc')).toBe(false);
+});
+
+test('123 is not a strong valid password', () => {
+    expect(functions.isStrongPassword('123')).toBe(false);
+});
+
+// Date validation tests
+test('05/28/2023 is a valid date', () => {
+    expect(functions.isDate('05/28/2023')).toBe(true);
+});
+
+test('5/5/5555 is a valid date', () => {
+    expect(functions.isDate('5/5/5555')).toBe(true);
+});
+
+test('5/5/55 is not a valid date', () => {
+    expect(functions.isDate('5/5/55')).toBe(false);
+});
+
+test('222/333/55555 is not a valid date', () => {
+    expect(functions.isDate('222/333/55555')).toBe(false);
+});
+
+// Hex color validation tests
+test('#000000 is a valid hex color', () => {
+    expect(functions.isHexColor('#000000')).toBe(true);
+});
+
+test('#FFFFFF is a valid hex color', () => {
+    expect(functions.isHexColor('#FFFFFF')).toBe(true);
+});
+
+test('cyan is not a valid hex color', () => {
+    expect(functions.isHexColor('cyan')).toBe(false);
+});
+
+test('#blackout is not a valid hex color', () => {
+    expect(functions.isHexColor('#blackout')).toBe(false);
+});
